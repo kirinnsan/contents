e57eff6998503ddac2a6d432ee376e08d850dc50
Hiroshi Takaichi <hirosi.t822@gmail.com>
2020-04-12 17:03:54 +0900

    Add log  settings

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let accessLogger = require("./lib/log/accesslogger.js");
+let systemLogger = require("./lib/log/systemlogger.js");
 let express = require("express");
 let app = express();
 
@@ -7,7 +9,11 @@ app.disable("x-powered-by");
 app.use("/public", express.static(__dirname + "/public/" +
   (process.env.NODE_ENV === "development" ? "development" : "production")));
 
+app.use(accessLogger());
+
 app.use("/", require("./routes/index.js"));
+
+app.use(systemLogger());
 
 app.listen(3000, () => {
   console.log("start server...");
---
+++ config/log4js.config.js
@@ -0,0 +1,47 @@
+let path = require("path");
+let root = path.join(__dirname, "../");
+
+module.exports = {
+  appenders: {
+    ConsoleLogAppender: {
+      type: "console"
+    },
+    FileLogAppender: {
+      type: "file",
+      filename: path.join(root, "./log/system/system.log"),
+      maxLogSize: 5000000,
+      bakups: 10
+    },
+    MuitiFileAppender: {
+      type: "multiFile",
+      base: path.join(root, "./log/application/"),
+      property: "key",
+      extension: ".log"
+    },
+    DateRollingFileAppender: {
+      type: "datefile",
+      filename: path.join(root, "./log/access/access.log"),
+      pattern: "-yyyyMMdd",
+      daysToKeep: 30
+    }
+
+  },
+  categories: {
+    "default": {
+      appenders: ["ConsoleLogAppender"],
+      level: "ALL"
+    },
+    system: {
+      appenders: ["FileLogAppender"],
+      level: "ERROR"
+    },
+    application: {
+      appenders: ["MuitiFileAppender"],
+      level: "ERROR"
+    },
+    access: {
+      appenders: ["DateRollingFileAppender"],
+      level: "INFO"
+    }
+  }
+};
