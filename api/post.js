2f0397c0fba3ba5b4e988f879e8fbe2242c3ab2b
Hiroshi Takaichi <hirosi.t822@gmail.com>
2020-04-22 23:07:37 +0900

    Add WebAPI of sample

---
+++ api/post.js
@@ -0,0 +1,23 @@
+let { CONNECTION_URL, OPTIONS, DATABASE } = require("../config/mongodb.config");
+let router = require("express").Router();
+let MongoClient = require("mongodb").MongoClient;
+
+router.get("/*", (req, res) => {
+  MongoClient.connect(CONNECTION_URL, OPTIONS, (error, client) => {
+    let db = client.db(DATABASE);
+    db.collection("posts").findOne({
+      url: req.url
+    }, {
+      projection: { _id: 0 } // _idの非表示
+    }
+    ).then((doc) => {
+      res.json(doc);
+    }).catch((errpr) => {
+      throw error;
+    }).finally(() => {
+      client.close();
+    })
+  });
+});
+
+module.exports = router;
