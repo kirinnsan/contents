52380112a994061e23d7c3da9c652e3839103129
Hiroshi Takaichi <hirosi.t822@gmail.com>
2020-04-24 06:48:09 +0900

    Add error page

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use("/api", (() => {
 
 app.use("/", (() => {
   let router = express.Router();
-  router.use((req,res,next)=> {
+  router.use((req, res, next) => {
     res.setHeader("X-Frame-Options", "SAMEORIGIN");
     next();
   });
@@ -50,6 +50,43 @@ app.use("/", (() => {
 })());
 
 app.use(systemLogger());
+
+app.use((req, res, next) => {
+  let data = {
+    method: req.method,
+    protocal: req.protocol,
+    version: req.httpVersion,
+    url: req.url
+  };
+  res.status(404);
+  // ajaxの場合
+  if (req.xhr) {
+    res.json(data);
+  } else {
+    res.render("./404.ejs", { data });
+  }
+});
+
+app.use((err, req, res, next) => {
+  let data = {
+    method: req.method,
+    protocal: req.protocol,
+    version: req.httpVersion,
+    url: req.url,
+    error: (process.env.NODE_ENV === "development") ? {
+      name: err.name,
+      message: err.message,
+      stack: err.stack
+    } : undefined
+  };
+  res.status(500);
+  // ajaxの場合
+  if (req.xhr) {
+    res.json(data);
+  } else {
+    res.render("./500.ejs", { data });
+  }
+});
 
 app.listen(3000, () => {
   console.log("start server...");
